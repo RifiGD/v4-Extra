cdc031a27e34e18420c426f468879e220b9af460
RifiGD <151682890+RifiGD@users.noreply.github.com>
2025-11-03 09:51:52 -0500

    add Profiles.hpp

---
+++ RSDKv4/Profiles.hpp
@@ -0,0 +1,20 @@
+// RSDKv4/Profiles.hpp
+#pragma once
+#ifndef PROFILES_H
+#define PROFILES_H
+#include <vector>
+#include <string>
+
+namespace SpecialProfiles {
+    const std::vector<std::string> AmazonHashes = {
+        "fed2403f352186e7ab8e5211ad9a5947266df1e5876f0cd7dc342d26e557fbb9",
+        "3b2346d4bda49ee09880dfdfc42cc40d70c3a932a4ade06087b8072756630b14"
+    };
+
+    const std::vector<std::string> ClassicsHashes = {
+        "90acea9924151240db3b7ed991d6424c502b19fc66237bce6030f190eafc7d64",
+        "a5c6f829b61a10ccd342e6da985c18f7fb74e3237a35b117e25975f96c42ab80",
+        "1d4cbc3c8e3021f2dc8eed972460a1e2566b4e757f90b2c98b00dc1a1440843a"
+    };
+}
+#endif
